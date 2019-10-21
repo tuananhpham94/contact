c6938a8b3812fd8088dfdfe00c5a740412093d2e
unknown <Anh>
2019-10-22 11:44:28 +1300

    first commit

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Request;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+//    /**
+//     * Login username to be used by the controller.
+//     *
+//     * @var string
+//     */
+//    protected $username;
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+//        $this->username = $this->findUsername();
+    }
+    /**
+     * Get the login username to be used by the controller.
+     *
+     * @return string
+     */
+//    public function findUsername(){
+//        $login = request()->input('login');
+////        $fieldType = filter_var($login, FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
+//        $fieldType = 'username';
+//        request()->merge([$fieldType => $login]);
+//        dd($login);
+//        return $fieldType;
+//    }
+    public function username() {
+        return 'username';
+    }
+
+}
